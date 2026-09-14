cb51f4f13491d466909901833accf591caf99488
Gilbert Horne Jr <gilhornejr@MacBookPro.lan>
2026-09-14 13:38:13 -0400

    update : new png image, rag.py modified

---
+++ neo4j_genAI/rag.py
@@ -0,0 +1,21 @@
+# RETRIEVAL AUGMENTED GENERATION (RAG)
+
+#  - an approach that enhances the responses of LLMs by providing them with relevent, up-to-date information retrieved from external sources.
+
+# RAG helps generate more accurate and tailored responses, especially when the required information is not present in the LLM's training data.
+
+# --- PROCESS --- 
+
+# The RAG process typically involves three main steps:
+
+# Understanding the User Query : The system first interprets the user’s input or question to determine what information is needed.
+
+# Information Retrieval : A retriever searches external data sources (such as documents, databases, or knowledge graphs) to find relevant information based on the user’s query.
+
+# Response Generation : The retrieved information is inserted into the prompt, and the language model uses this context to generate a more accurate and relevant response.
+
+# --- GROUNDING ---
+
+# The process of providing context to an LLM to improve the accuracy of its responses and reduce the likelihood of hallucinations.
+
+
