24b1cf1cbbfd2a208c9d87ef4c22651a149469c9
Gilbert Horne Jr <gilhornejr@MacBookPro.lan>
2026-09-14 18:15:12 -0400

    modified : rag.py file

--- neo4j_genAI/rag.py
+++ neo4j_genAI/rag.py
@@ -18,4 +18,45 @@
 
 # The process of providing context to an LLM to improve the accuracy of its responses and reduce the likelihood of hallucinations.
 
+# --- RETRIEVERS ---
+
+# The retreiver is a key component of RAG process. 
+# A retriever is responsible for searching and retrieving relevant information from external sources based on the user’s query.
+
+# Neo4j supports various methods fro building retrievers:
+
+# - Full-text search
+# - Vector search
+# - Text to Cypher
+
+# --- DATA SOURCES ---
+
+# Documents - Textual data sources, such as articles, reports, or manuals, that can be searched for relevant information.
+
+# APIs - External services that can provide real-time data or specific information based on user queries.
+
+# Knowledge Graphs - Graph-based respresentations of information that can provide context and relationships between entities.
+
+# --- VECTOR RAG ---
+
+# --- SEMANTIC SEARCH ---
+
+# Semantic search aims to understand search phrases' intent and contextual meaning, rather than focusing on individual keywords.
+
+# The results are tailored based on the term and the perceived intent.
+
+# --- VECTORS ---
+
+# You can represent data as vectors to perform semantic search.
+# You can use vectors to represent many different types of data, including text, images, and audio.
+
+# Vector dimensionality refers to the number of dimensions in a vector. 
+
+# Higher dimensionality captures more fine-grained meaning but is more expensive computationally and similarly. 
+
+# Lower dimensionality is faster and cheaper to compute, but offers less nuance.
+
+# -- EMBEDDINGS --
+
+
 
